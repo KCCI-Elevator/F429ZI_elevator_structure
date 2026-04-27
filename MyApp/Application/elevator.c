0c7ed5063e75fea05494efe35e87229f4cb77f5f
bisa16x2 <bisa16x2@gmail.com>
2026-04-27 20:30:44 +0900

    first commit

--- MyApp/Application/elevator.c
+++ MyApp/Application/elevator.c
@@ -1,5 +1,19 @@
 #include "elevator.h"
 
+// inner
+static void elevatorSetState(elevator_t *car, elevator_state_t state, uint32_t now){
+    car->state = state;
+    car->state_time = now;
+}
+
+static uint8_t elevatorFindNextTarget(elevator_t *car){
+    for (uint8_t floor = 1; floor <= ELEVATOR_FLOOR_MAX; floor++){
+        return floor;
+    }
+    return 0;
+}
+
+// function
 void elevatorInit(){
     
 }
