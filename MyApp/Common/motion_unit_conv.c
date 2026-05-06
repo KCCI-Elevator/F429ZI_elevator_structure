0a68533d5c798438900fc713ac669886839e9f20
jashon <rlarkdmf6141@gmail.com>
2026-05-06 14:49:24 +0900

    unit_conv 수치 수정

--- MyApp/Common/motion_unit_conv.c
+++ MyApp/Common/motion_unit_conv.c
@@ -1,8 +1,8 @@
 #include "motion_unit_conv.h"
 
-// 엔코더 11000 pulses == 약 520 mm
+// 엔코더 11000 pulses == 약 500 mm
 #define REF_PULSE_FOR_MM  11000.0f
-#define REF_DISTANCE_MM      520.0f
+#define REF_DISTANCE_MM      500.0f
 
 float motionPulseToMM(float pulse) {
     return pulse * (REF_DISTANCE_MM / REF_PULSE_FOR_MM);
